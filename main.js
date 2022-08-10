8e823c9e80c40a94735225b6005ce8d8378bf97c
hfutgf <beretax308@gmail.com>
2022-08-10 20:44:44 +0500

    asdf

---
+++ main.js
@@ -0,0 +1,81 @@
+// var summa = Number(prompt("summani kiriting" +' ' + '$'));
+
+// if (summa.value < 850 ) {
+//    console.log('sizning pulingiz yetarli emas');
+// } else (summa.value >= 850 )
+// {
+//    console.log('sizning pulingiz yetarli');
+// }
+
+var summa = Number(prompt("summani kiriting" + " " + "$"));
+
+if (summa < 870) {
+  alert("sizda nixuya pul yo");
+ 
+} else {
+  alert("sizda pul kop");
+}
+setTimeout(() => {
+    salom()
+}, 2000);
+// if (summa.value < 870 ) {
+//     (alert = summa1);
+// }
+//  else (summa.value > 870) {
+//     (alert = summa2);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function salom() {
+  for (let i = 0; i > -1; i++) {
+    console.log("Jasur browser console liga qara" + " " + i);
+  }
+}
